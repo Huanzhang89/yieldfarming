accb68481076a28af8e6b96ee7203c7ee91fc535
Huan Zhang <huanzhang30@gmail.com>
2020-08-18 19:34:11 +0100

    Updates

---
+++ js/homepage.55e3eee29eea8fe2443ff7c8b1353d52.js
@@ -0,0 +1,12 @@
+/*!
+* YieldFarming
+* Boilerplate for a Static website using EJS and SASS
+* https://https://huanzhang89.github.io/yieldfarming/
+* @author Jongseung Lim -- https://yieldfarming.info
+* Copyright 2020. MIT Licensed.
+*/
+
+$(async function() {
+
+});
+
